868acb754246b0093acca5ea91e281f4d37030ee
Fadhila Zalfa Alisya <zalfa.alisya@gmail.com>
2023-03-03 07:10:46 +0700

    dev:update hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <section className="bg-[#ffd1ac] dark:text-gray-100">
+    <section className="bg-[#d1d1d1] dark:text-gray-100">
 	<div className="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl">
 		<h1 className="text-4xl font-bold leading-none sm:text-5xl">Hello, welcome to 
 			<span className="dark:text-violet-400">ELAYNE</span>
